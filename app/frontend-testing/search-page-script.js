4377c3f555dbcc6d85e18ab19fbdfd6220b8c871
CalShark <callumshark@yahoo.com>
2022-11-06 17:28:57 -0500

    testing API with front-end

--- app/frontend-testing/search-page-script.js
+++ app/frontend-testing/search-page-script.js
@@ -3,16 +3,39 @@ const searchButton = document.querySelector(".search-btn")
 
 const textInputLocation = document.getElementById(".text-input-location")
 
-searchButton.addEventListener('click', ()=> {
-    console.log("search button clicked")
-    //console.log(textInputLocation.value);
+var createLineItem = function (item) {
+    return '<div>' + item.title + ' ' + item.location + '</div>';
+}
 
-    fetch('http://localhost:8080/api/properties')
+$(document).ready(function () {
+    $(searchButton).click(function () {
+
+        console.log("search button clicked");
+
+        $.ajax({
+            url: "http://localhost:8080/api/properties", success: function (result) {
+                debugger;
+                var listOfItems = '';
+
+                for (var x = 0; x < result.length; x++) {
+                    listOfItems += createLineItem(result[x]);
+                }
+
+                $('#div1').html(listOfItems);
+            }
+        });
+    });
+});
+
+searchButton.addEventListener('click', ()=> {
+    //console.log("search button clicked")
+
+    /*fetch('http://localhost:8080/api/properties')
         .then(data => {
             data.forEach(property => {
                 const title = `<h3>` + property.title + `</h3>` //or is it property.id?
                 propertyDisplay.insertAdjacentElement("beforeend", title)
             })
-      })
+      })*/
 });
 
